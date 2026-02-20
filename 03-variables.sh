7ad8ba7f842a6a708ad3d6401574a637cb06fcb4
shalini-9393 <shalinithatikonda73@gmail.com>
2026-02-20 11:48:26 +0530

    Revise conversation in 03-variables.sh

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-PERSON1=APPLE
-PERSON2=BUDDY
-
-echo "$PERSON1:: Hello $PERSON2, How are you?"
-echo "$PERSON2:: Hi $PERSON1, I am fine. Thanks for asking. How are you doing?"
-echo "$PERSON1:: I am doing good. What are you learning $PERSON2?"
-echo "$PERSON2:: I am learning shell scripting. It is really interesting. What about you $PERSON1?"
-echo "$PERSON1:: I am preparing for my JE exam. It is really tough, but I am trying my best."
+echo "apple:: hello buddy, how are you?"
+echo "buddy:: hi apple, iam fime. thanks for asking. how are you doing?"
+echo "apple:: iam doing good. what are you learning buddy?"
+echo "buddy:: iam learning shell what about you apple?"
