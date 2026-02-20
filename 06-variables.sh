e816559e3cf207b6182ed1f03836ef19343de599
Shalini Sirisha <your-github-email@example.com>
2026-02-20 13:31:29 +0530

    actions

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+START_TIME=$(date +%s)
 
-echo "script executed at: $TIMESTAMP" 
+echo "script executed at: $START_TIME"
+
+sleep 10
+END_TIME=$(date +%s)  
+
+ELAPSED_TIME=$((END_TIME - START_TIME))
+
+echo "script executed at: $END_TIME"
+echo "elapsed time: $ELAPSED_TIME seconds"
