7d95eb241b233f57a61a55a7ea6a8a921c487e7d
Shalini Sirisha <your-github-email@example.com>
2026-02-20 13:14:58 +0530

    actions

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,3 +4,8 @@ echo "please Enter your username::"
 read USER_NAME
 
 echo user name is $USER_NAME
+
+echo "please Enter your password::"
+read -s USER_PASSWORD
+
+echo "your password is $USER_PASSWORD"
