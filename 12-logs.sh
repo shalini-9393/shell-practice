5aad72fdd740f1cdff55df6c0cee3ef9276d4768
Shalini Sirisha <your-github-email@example.com>
2026-02-23 12:26:16 +0530

    actions

--- 12-logs.sh
+++ 12-logs.sh
@@ -5,17 +5,17 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERid -ne 0 ]; then
-    echo "Please run this script as root or with sudo."
+    echo "Please run this script as root or with sudo." | tee -a $LOGS_FILE
     exit 1
 fi
 mkdir -p $LOGS_FOLDER
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-        echo "$2 installing failed."
+        echo "$2 installing failed." | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2 installing successfully."
+        echo "$2 installing successfully." | tee -a $LOGS_FILE
     fi
 }
 
