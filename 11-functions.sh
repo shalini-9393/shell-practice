7ca8656922019e3182eefecdae4ce9bc3dcdbd37
Shalini Sirisha <your-github-email@example.com>
2026-02-23 11:42:14 +0530

    actions

---
+++ 11-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERid=$(id -u)
+
+if [ $USERid -ne 0 ]; then
+    echo "Please run this script as root or with sudo."
+    exit 1
+fi
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo "$2 installing failed."
+        exit 1
+    else
+        echo "$2 installing successfully."
+    fi
+}
+
+dnf install nginx -y
+VALIDATE $? installing NGINX
+
+dnf install mysql-sever -y
+VALIDATE $? installing MySQL 
+
+dnf install nodejs -y
+VALIDATE $? installing NodeJS
